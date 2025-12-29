84756f1fc47fe4230c339fcf9f2470a293754a41
Gregory Green <green_gregory@yahoo.com>
2025-12-29 12:25:15 -0500

    adding Serialization fix

--- applications/patterns/caching/services/employee-caching-service/src/main/java/spring/design/data/patterns/employee/ValkeyConfig.java
+++ applications/patterns/caching/services/employee-caching-service/src/main/java/spring/design/data/patterns/employee/ValkeyConfig.java
@@ -1,5 +1,6 @@
 package spring.design.data.patterns.employee;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -7,10 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import tools.jackson.databind.ObjectMapper;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.*;
 
 import java.time.Duration;
 
@@ -25,24 +24,23 @@ public class ValkeyConfig {
     @Value("${app.cache.ttl.seconds:10}")
     private long ttlSeconds;
 
-    @Bean
-    public RedisSerializer<String> serializer()
-    {
-        return  (RedisSerializer) new GenericJacksonJsonRedisSerializer(new ObjectMapper());
-    }
+//
+//    @Bean
+//    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+//
+////        RedisSerializationContext<String, String> redisSerializationContext = RedisSerializationContext.fromSerializer(serializer);
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.findAndRegisterModules();
+//        var cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+////                .entryTtl(Duration.ofSeconds(ttlSeconds))
+//                .serializeKeysWith(redisSerializationContext.getKeySerializationPair())
+//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper))
+//                )
+//                .disableCachingNullValues();
+//
+//        return  RedisCacheManager.builder(connectionFactory).cacheDefaults(cacheConfiguration)
+//                .build();
+//    }
 
-    @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory, RedisSerializer<String> serializer) {
-
-        RedisSerializationContext<String, String> redisSerializationContext = RedisSerializationContext.fromSerializer(serializer);
-
-        var cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofSeconds(ttlSeconds))
-                .serializeKeysWith(redisSerializationContext.getKeySerializationPair())
-                .serializeValuesWith(redisSerializationContext.getValueSerializationPair())
-                .disableCachingNullValues();
-
-        return  RedisCacheManager.builder(connectionFactory).cacheDefaults(cacheConfiguration)
-                .build();
-    }
 }
--- components/domains/src/main/java/spring/data/patterns/employee/domains/employee/records/Employee.java
+++ components/domains/src/main/java/spring/data/patterns/employee/domains/employee/records/Employee.java
@@ -2,6 +2,7 @@ package spring.data.patterns.employee.domains.employee.records;
 
 import lombok.Builder;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
  * @author Gregory Green
  */
 @Builder
-public record Employee(String id, String empName, String jobName, LocalDateTime hireDate, BigDecimal salary, BigDecimal commission, String department, String managerId) {
+public record Employee(String id, String empName, String jobName, LocalDateTime hireDate, BigDecimal salary, BigDecimal commission, String department, String managerId) implements Serializable {
     public Employee {
         java.util.Objects.requireNonNull(id, "id required");
         java.util.Objects.requireNonNull(empName, "employee name required");
