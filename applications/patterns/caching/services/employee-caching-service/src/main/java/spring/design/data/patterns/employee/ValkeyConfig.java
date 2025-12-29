e6afc8c6d445670e63e22a93117fe048e41e229a
Gregory Green <green_gregory@yahoo.com>
2025-12-29 12:46:40 -0500

    adding JSON serialization

--- applications/patterns/caching/services/employee-caching-service/src/main/java/spring/design/data/patterns/employee/ValkeyConfig.java
+++ applications/patterns/caching/services/employee-caching-service/src/main/java/spring/design/data/patterns/employee/ValkeyConfig.java
@@ -1,17 +1,13 @@
 package spring.design.data.patterns.employee;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.*;
-
-import java.time.Duration;
+import tools.jackson.databind.ObjectMapper;
 
 /**
  * @author Gregory Green
@@ -24,23 +20,20 @@ public class ValkeyConfig {
     @Value("${app.cache.ttl.seconds:10}")
     private long ttlSeconds;
 
-//
-//    @Bean
-//    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-//
-////        RedisSerializationContext<String, String> redisSerializationContext = RedisSerializationContext.fromSerializer(serializer);
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.findAndRegisterModules();
-//        var cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-////                .entryTtl(Duration.ofSeconds(ttlSeconds))
-//                .serializeKeysWith(redisSerializationContext.getKeySerializationPair())
-//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper))
-//                )
-//                .disableCachingNullValues();
-//
-//        return  RedisCacheManager.builder(connectionFactory).cacheDefaults(cacheConfiguration)
-//                .build();
-//    }
 
+    /**
+     * Use JSON serialization for Redis cache values
+     * @param objectMapper the object mapper
+     * @return the redis cache manager customizer
+     */
+    @Bean
+    public RedisCacheManagerBuilderCustomizer myRedisCacheManagerBuilderCustomizer(ObjectMapper objectMapper) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair.fromSerializer(new GenericJacksonJsonRedisSerializer(objectMapper)));
+
+        return (builder) -> builder
+                .cacheDefaults(cacheConfig);
+    }
 }
--- components/domains/src/main/java/spring/data/patterns/employee/domains/employee/records/Employee.java
+++ components/domains/src/main/java/spring/data/patterns/employee/domains/employee/records/Employee.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
  * @author Gregory Green
  */
 @Builder
-public record Employee(String id, String empName, String jobName, LocalDateTime hireDate, BigDecimal salary, BigDecimal commission, String department, String managerId) implements Serializable {
+public record Employee(String id, String empName, String jobName, LocalDateTime hireDate, BigDecimal salary, BigDecimal commission, String department, String managerId) {
     public Employee {
         java.util.Objects.requireNonNull(id, "id required");
         java.util.Objects.requireNonNull(empName, "employee name required");
